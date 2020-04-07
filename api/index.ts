800476ede9e15fbc909772ebc9862f199173c072
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2020-04-07 10:31:38 +0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -18,27 +18,27 @@ export default async (_, response: NowResponse) => {
   response.json({
     confirmed: {
       value: confirmed,
-      detail: "https://covid19.mathdro.id/api/confirmed"
+      detail: "https://covid-19-api-vert.now.sh/api/confirmed"
     },
     recovered: {
       value: recovered,
-      detail: "https://covid19.mathdro.id/api/recovered"
+      detail: "https://covid-19-api-vert.now.sh/api/recovered"
     },
     deaths: {
       value: deaths,
-      detail: "https://covid19.mathdro.id/api/deaths"
+      detail: "https://covid-19-api-vert.now.sh/api/deaths"
     },
-    dailySummary: "https://covid19.mathdro.id/api/daily",
+    dailySummary: "https://covid-19-api-vert.now.sh/api/daily",
     dailyTimeSeries: {
-      pattern: "https://covid19.mathdro.id/api/daily/[dateString]",
-      example: "https://covid19.mathdro.id/api/daily/2-14-2020"
+      pattern: "https://covid-19-api-vert.now.sh/api/daily/[dateString]",
+      example: "https://covid-19-api-vert.now.sh/api/daily/2-14-2020"
     },
-    image: "https://covid19.mathdro.id/api/og",
-    source: "https://github.com/mathdroid/covid19",
-    countries: "https://covid19.mathdro.id/api/countries",
+    image: "https://covid-19-api-vert.now.sh/api/og",
+    source: "https://github.com/web-sys1/covid-19-api/",
+    countries: "https://covid-19-api-vert.now.sh/api/countries",
     countryDetail: {
-      pattern: "https://covid19.mathdro.id/api/countries/[country]",
-      example: "https://covid19.mathdro.id/api/countries/USA"
+      pattern: "https://covid-19-api-vert.now.sh/api/countries/[country]",
+      example: "https://covid-19-api-vert.now.sh/api/countries/USA"
     },
     lastUpdate
   });
